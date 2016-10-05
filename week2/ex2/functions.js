aa2a268b479ea6b4956658eb64ff89d0327b3a7f
wallpocketsbyaviva <aviva.r.friedman@gmail.com>
2016-10-05 00:45:51 -0400

    not sure what this is

---
+++ week2/ex2/functions.js
@@ -0,0 +1,16 @@
+
+
+var add2= function(num1){
+	var solution = num1+2
+
+	return solution;
+}
+
+
+
+console.log (add2(3))
+
+console.log (add2(200));
+
+
+//this is saying there is now a function that adds 2 to whatever number you put in) 
--- week2/ex2/stock.js
+++ week2/ex2/stock.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-var stockPrice = 10; //assume dollars
+/*
+Adam Driggers [7:41 PM]   EX2 You have just invested in ADDA, a sweet digital
+design agency. The stock price starts at 10 dollars. While the stock price is
+greater than or equal to 10, confirm the new stock price. If the stock price
+falls below 10 dollars, alert the user that they should sell! Stop asking what
+the price is.*/
 
-while (stockPrice >= 10){
 
-	stockPrice = parseInt(prompt("What's ADDA's stock price now?")); //won't work - why? Spend 2 minutes discussing this with your partner
 
-	if (stockPrice < 10){
-		alert('Sell!!!');
-	}
-}
-=======
-console.log('working!');
->>>>>>> ADDA-js/master
-=======
-console.log('working!');
->>>>>>> ADDA-js/master
